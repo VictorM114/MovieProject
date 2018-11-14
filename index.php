caaad4f5da26332a9b20abadcf0c13ab1615e6ca
Victor Ayala <victor.ayala1@upr.edu>
2018-11-14 17:49:41 -0500

    Ultimo edit

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Movie Time</title><!--Titulo de la pagina-->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all"> <!--Implementacion de los css que ya estaban -->
+</head>
+<body id="top">
+<!-- Top Background Image Wrapper -->
+<div class="bgded overlay" style="background-image:url('images/demo/backgrounds/bluelines.jpg');"> 
+  <!-- ################################################################################################ -->
+  <div class="wrapper row0">
+    <div id="topbar" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div class="fl_right">
+        <ul class="nospace">
+          <!--<li><a href="index.php"><i class="fa fa-lg fa-home"></i></a></li>-->
+          <li><a href="#">Login / Sign In</a></li>
+          <li><a href="#">Register</a></li>
+        </ul>
+      </div>
+      <!-- ################################################################################################ -->
+    </div>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+   <div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <!-- ################################################################################################ -->
+    </header>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div id="pageintro" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <article class="introtxt">
+      	<h2 class="heading">Join our cinephile family</h2>
+        <h2 class="heading">Movie Time</h2>	
+		<p>Do you love movies as much as we do? <br>Here you can rate, review, search, debate, discover and much more. </p>
+      <footer><a class="btn" href="#">Register</a></footer>
+    </article>
+    <!-- ################################################################################################ -->
+    <div class="clear"></div>
+  </div>
+  <!-- ################################################################################################ -->
+</div>
+<!-- End Top Background Image Wrapper -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="group center">
+      <article class="one_quarter first"><i class="icon fa fa-repeat"></i>
+        <h4 class="font-x1"><a href="#">Watch Later</a></h4>
+        <p>You can add movies to a watch later list so you won't forget to watch them. Also you can 
+			add them in order of priority.</p>
+      </article>
+      <article class="one_quarter"><i class="icon fa fa-bar-chart"></i>
+        <h4 class="font-x1"><a href="pages/toprated.php"">Top Rated</a></h4>
+        <p>You can rate the movies that you watch and see what are the best and worse rated movies in our app.</p>
+      </article>
+      <article class="one_quarter"><i class="icon fa fa-hashtag"></i>
+        <h4 class="font-x1"><a href="#">Recomendations</a></h4>
+        <p>You can have recomended movies depending on the genre, director and casting on the movies you watch.</p>
+      </article>
+	    <article class="one_quarter"><i class="icon fa fa-bitbucket"></i>
+        <h4 class="font-x1"><a href="#">Socialize</a></h4>
+        <p>You can talk with other users, see the movies they are watching or want to watch.</p>
+      </article>
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper">
+  <div class="split clear" style="background-image:url('images/demo/backgrounds/bluelines.jpg');">
+    <section class="box"> 
+      <!-- ################################################################################################ -->
+      <div class="btmspace-50">
+        <h3 class="font-x2 nospace">Your Profile</h3>
+        <p class="nospace"> This is the things you will have in your Movie Time account </p>
+      </div>
+      <ul class="nospace group services">
+        <li class="one_half first">
+          <article><a href="#"><i class="fa fa-plug"></i></a>
+            <h6 class="heading">Watched Movies</h6>
+            <p>In your profile keep track of all the movies you've watched.</p>
+          </article>
+        </li>
+        <li class="one_half">
+          <article><a href="#"><i class="fa fa-puzzle-piece"></i></a>
+            <h6 class="heading">Following and Followers</h6>
+            <p>You can follow your friends that have a Movie Time account. If they don't have one you can invite them.</p>
+			</article>
+        </li>
+        <li class="one_half first">
+          <article><a href="#"><i class="fa fa-random"></i></a>
+            <h6 class="heading">Recomendations</h6>
+            <p>You will have recommended movies depending on the genre, director and casting on the movies you watch. </p>
+          </article>
+        </li>
+        <li class="one_half">
+          <article><a href="#"><i class="fa fa-firefox"></i></a>
+            <h6 class="heading">To Watch Movies</h6>
+            <p>You will have the list of movies you want to watch. </p>
+			</article>
+        </li>
+      </ul>
+      <!-- ################################################################################################ -->
+    </section>
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <section class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="center btmspace-50">
+      <h3 class="font-x2 nospace">Sneak Peak of Movie Time</h3>
+      <p class="nospace">This a sneak peak of our app if you aren't convinced yet.</p>
+    </div>
+    <div class="group">
+      <article class="one_third first"><a href="#"><img class="btmspace-30" src="images/demo/infinitywar.jpg" alt=""></a>
+        <time class="block font-xs" datetime="2045-04-06">23<sup>rd</sup> April 2018</time>
+        <h4 class="nospace font-x1">Avengers Infinity War</h4>
+        <p>Most viewed movie of all time by Movie Time users.</p>
+      </article>
+      <article class="one_third"><a href="#"><img class="btmspace-30" src="images/demo/LordOfTheRings.jpg" alt=""></a>
+        <time class="block font-xs" datetime="2045-04-05">17<sup>th</sup> December 2003</time>
+        <h4 class="nospace font-x1">Lord of The Rings: The Return of The King</h4>
+        <p>Best Rated movie of all time by Movie Time users.</p>
+      </article>
+      <article class="one_third"><a href="#"><img class="btmspace-30" src="images/demo/airbender.jpg" alt=""></a>
+        <time class="block font-xs" datetime="2045-04-04">30<sup>th</sup> June 2010</time>
+        <h4 class="nospace font-x1">The Last Airbender</h4>
+        <p>Worst Rated movie of all time by Movie Time users.</p>
+      </article>
+    </div>
+    <!-- ################################################################################################ -->
+  </section>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved Victor and Alice - <a href="#">SICI4997</a></p
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
